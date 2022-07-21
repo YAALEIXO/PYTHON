68ee99e9afa32d4efc3a00ff3bda149e8001620f
Yuri Aleixo <yuriantonov@yahoo.com.br>
2022-07-21 00:17:06 -0300

    while ok

--- Cadastro evento.py
+++ Cadastro evento.py
@@ -1,48 +1,42 @@
+#-----------------VARIAVEIS----------#
 import pandas as pd
 Cod = []
 Nome = []
 Idade = []
 Contato = []
-cadastro = {"Id": Cod, "Nome": Nome, "Idade": Idade, "Telefone": Contato}
-print("Sejam bem vindo ao cadastro de eventos")
+Lista = {"Id": Cod, "Nome": Nome, "Idade": Idade, "Telefone": Contato}
+cadastro =[]
+#-----------CADASTRO-----------------#
+seunome=(input("Olá qual seu nome"))
+print( seunome, 'estamos prestes a iniciar o cadastro de sua lista:')
+New = print('')
+#---------1º CADASTRO---------------#
+while New !='sS'or New !='nN':
+    New = input('Deseja continuar cadstro? s ou n?  ' )
 
-print("vamos começar")
-
-Nome.append(input("qual seu nome"))
-Idade.append(input("qual sua idade"))
-Contato.append(input("Contato"))
-New = input("Deseja realizar primeiro cadastro S ou N")
-if New in "sS": Cod.append(len(Cod) + 1)
-
-
-while New:
-    if New != "sS" or New !='nN':
-        New = input("Deseja realizar outro cadastro? S ou N")
-        if New in 'sS':
-            break
-            if New in "sS": Cod.append(len(Cod) + 1)
-        elif New in 'nN':
-            Cod.append(len(Cod)+1)
-            print( 'Fim')
-            break
-    else:
-        print('fim do mundo')
-
-
-while len(Cod) < 5 and New in "sS":
-
-    Nome.append(input("qual seu nome"))
-    Idade.append(input("qual sua idade"))
-    Contato.append(input("Contato"))
-    New = input("Deseja realizar outro cadastro? S ou N")
-    if New in "sS" and len(Cod) <5: Cod.append(len(Cod) + 1)
-
-    else:
-        print("Sua Lista esta incompleta")
+    if New =='N' or New=='n':
         break
 
-else:
+    if New =='S' or New=='s':
+        Cod.append(len(Cod)+1)
+        Nome.append(input("qual seu nome"))
+        Idade.append(input("qual sua idade"))
+        Contato.append(input("Contato"))
+    if len(Cod)==2:
+        print ('Lista Completa')
+        break
+#------------Tabela de cadastro -------#
+print('Finalizamos aqui!')
+Tabela1 = pd.DataFrame (Lista)
+#-------Tabela--------------
+if len(Cod)!=0:
+    print(Tabela1)
 
-    print(" ")
-tabela = pd.DataFrame(cadastro)
-print(tabela)
+
+
+
+
+
+
+
+
