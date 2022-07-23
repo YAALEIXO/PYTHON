467a33d1a4da944c095397b0a0fa110883f55287
Yuri Aleixo <yuriantonov@yahoo.com.br>
2022-07-23 10:49:18 -0300

    Data Frame

---
+++ TABELA - DICIONARIO  - DATA FRAME.py
@@ -0,0 +1,8 @@
+import pandas as pd
+Tabela_nome = ['YURI','GUSTAVO','PEDRO','LUCAS','MARCOS']
+Tabela_idade =[25,8,50,18,25]
+Dicionario = {"Nome":Tabela_nome,"Idade":Tabela_idade}
+Dataframe = pd.DataFrame(Dicionario)
+print(Dataframe)
+
+# ISSO É TUDO PESSOAL DEIXE SEU LIKE E SEGUE PARA MAIS VIDEOS
